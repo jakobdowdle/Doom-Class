88b5c557b9a00444e162cd1caecd0147b60f45e7
Jakob Dowdle <52472328+jakobdowdle@users.noreply.github.com>
2024-11-22 16:16:00 -0700

    made simple character controller without vertical camera movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float lookSpeed = 2f;
+
+    private float xRotation = 0f;
+    [SerializeField] private Transform cameraTransform;
+
+    void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update() {
+        HandleMovement();
+        HandleMouseLook();
+    }
+
+    void HandleMovement() {
+        Vector3 moveDirection = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W)) {
+            moveDirection += transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            moveDirection -= transform.forward;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            moveDirection -= transform.right;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            moveDirection += transform.right;
+        }
+
+        transform.position += moveDirection.normalized * moveSpeed * Time.deltaTime;
+    }
+
+    void HandleMouseLook() {
+        float mouseX = Input.GetAxis("Mouse X") * lookSpeed;
+
+        // UNCOMMENT THESE LINES OF CODE FOR VERTICAL CAMERA
+        //float mouseY = Input.GetAxis("Mouse Y") * lookSpeed;
+        //xRotation -= mouseY;
+        //xRotation = Mathf.Clamp(xRotation, -90f, 90f); // Prevent over-rotation
+
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}

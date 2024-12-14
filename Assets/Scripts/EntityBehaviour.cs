128b91cce2951d92fd7a1d92e9e6cfbae392fd89
Jakob Dowdle <52472328+jakobdowdle@users.noreply.github.com>
2024-12-13 17:52:00 -0700

    made audio prefabs and added some to enemies and guns

--- Assets/Scripts/EntityBehaviour.cs
+++ Assets/Scripts/EntityBehaviour.cs
@@ -2,23 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EntityBehaviour : MonoBehaviour
-
-{
+public class EntityBehaviour : MonoBehaviour {
     [SerializeField] private int health;
+    [SerializeField] private AudioSource EnemyPain;
+    [SerializeField] private AudioSource EnemyShoot;
 
-    public void takeDamage(int dmg)
-    { 
-            health -= dmg;
+    private void Awake() {
+        if (EnemyPain == null) {
+            EnemyPain = GetComponentInChildren<AudioSource>();
+            if (EnemyPain == null) {
+                Debug.LogError("No AudioSource found.");
+            }
+        }
+        if (EnemyShoot == null) {
+            EnemyShoot = GetComponentInChildren<AudioSource>();
+            if (EnemyShoot == null) {
+                Debug.LogError("No AudioSource found.");
+            }
+        }
+    }
+
+    public void takeDamage(int dmg) {
+        health -= dmg;
         Debug.Log(health);
-        if (health < 1)
-        {
+
+        if (EnemyPain != null) {
+            EnemyPain.Play();
+        }
+
+        if (health < 1) {
             health = 0;
             Die();
         }
     }
-    void Die()
-    {
+
+    void Die() {
         gameObject.SetActive(false);
     }
 }
